f137dea38d299777b84f5c996824907f99d46573
DESKTOP-61RINEG\ユーザー <nemukoo@gmail.com>
2022-08-01 14:31:31 +0900

    test01

---
+++ src/main/java/com/example/demo/Test20220606Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test20220606Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test20220606Application.class, args);
+	}
+
+}
